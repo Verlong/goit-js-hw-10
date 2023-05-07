93507fffe8d597ae848733f7a740ca3c8a05b146
Maryna Bonko <bonko23@gmail.com>
2023-05-07 10:03:08 +0300

    library

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+// Notiflix.Notify.success('Sol lucet omnibus');
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+// Notiflix.Notify.warning('Memento te hominem esse');
+// Notiflix.Notify.info('Cogito ergo sum');
+const DEBOUNCE_DELAY = 300;
